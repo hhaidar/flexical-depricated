1642faa699fb01c046e1fd09276465dcd4ac78e0
Houssam Haidar <houssam@sdelements.com>
2012-12-08 22:11:11 -0600

    Add socket.io

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
 var express = require('express');
 var cons = require('consolidate');
 var swig = require('swig');
+var http = require('http');
 
-var app = express();
+var Flexical = function() {
 
-app.engine('.html', cons.swig);
-
-app.set('view engine', 'html');
-
-swig.init({
-    root: 'templates',
-    allowErrors: true
-});
-
-app.set('views', 'templates');
-
-app.use('/media', express.static(__dirname + '/media'));
-
-app.get('/', function (req, res) {
-    res.render('board.html', {
-        media_url: '/media'
+    var self = this;
+    
+    this.app = express();
+    
+    //
+    // Setup
+    //
+    this.app.configure(function() {
+        swig.init({
+            root: 'templates',
+            allowErrors: true
+        });
+        self.app.engine('.html', cons.swig);
+        self.app.set('view engine', 'html');
+        self.app.set('views', 'templates');
+        self.app.use('/media', express.static(__dirname + '/media'));
     });
-});
 
-app.listen(3000);
+    //
+    // Routes
+    //
+    this.app.get('/', function (req, res) {
+        res.render('board.html', {
+            media_url: '/media'
+        });
+    });
+
+    //
+    // Server
+    //
+    this.server = http.createServer(this.app);
+
+    //
+    // Socket IO
+    //
+    this.io = require('socket.io').listen(this.server);
+
+    this.io.set('transports', ['websocket'])
+    this.io.set('log level', 0);
+
+    this.io.sockets.on('connection', function(client) {
+
+        console.log('connected');
+
+    });
+    
+    //
+    // Listen
+    //
+    this.server.listen(3000);
+
+}
+
+var board = new Flexical();
+
--- media/js/board.js
+++ media/js/board.js
@@ -1,3 +1,20 @@
+
+var Flexical = function() {
+
+    var self = this;
+
+    var socket = io.connect();
+
+    socket.on('connect', function(data) {
+        console.log('connected');
+    });
+    
+    socket.on('widget:update', function(data) {
+        console.log(data);
+    });
+ 
+}
+
 $(function() {
-    // Nothing to see here...
+    var board = new Flexical();
 });
