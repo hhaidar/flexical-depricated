e4ace7039e140db841c15f48bb58bcb3b6e3db56
Houssam Haidar <houssam@sdelements.com>
2012-12-09 23:21:15 -0600

    Add basic server widget

--- media/js/views.js
+++ media/js/views.js
@@ -1,3 +1,20 @@
-$(function() {
-    // Nothing to see here...
+window.ServersView = Backbone.View.extend({
+    el: '[data-id="servers"]',
+    initialize: function() {
+        var self = this;
+        this.template = Handlebars.compile(this.$('[data-template]').html());
+        this.$servers = this.$('[data-list="servers"]');
+    },
+    update: function(data) {
+        var self = this;
+        _.each(data.servers, function(server) {
+            var html = self.template(server);
+            var $server = self.$servers.find('[data-id=' + server.id + ']');
+            if ($server.length > 0) {
+                $server.replaceWith(html);
+            } else {
+                self.$servers.append(html);
+            }
+        });
+    },
 });
---
+++ widgets.js
@@ -0,0 +1,42 @@
+var _ = require('underscore');
+var async = require('async');
+var request = require('request');
+
+var config = require('./config.js');
+
+module.exports = {
+    'servers': {
+        title: 'Server Status',
+        interval: 30 * 1000,
+        fetch: function(emitter) {
+            var requests = [];
+            var servers = config.servers;
+            _.each(servers, function(server, id) {
+                requests.push(function(callback) {
+                    var current = { 
+                        id: id,
+                        name: server.name,
+                        status: 'up'
+                    }
+                    request({
+                        uri: server.url,
+                        method: server.method || 'GET'
+                    }, function (err, res, body) {
+                        if (err || server.test && !server.test(res, body)) {
+                            // Well shit
+                            current.status = 'down'
+                            callback(null, current);
+                            return;
+                        }
+                        callback(null, current)
+                    });
+                });
+            });
+            async.series(requests, function(err, res) {
+                var data = {};
+                data.servers = res;
+                emitter(data);
+            });
+        }
+    }
+}
