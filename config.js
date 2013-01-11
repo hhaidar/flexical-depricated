4c32366ef56d2677d604e16473ac31982adeb32e
Houssam Haidar <houssam@sdelements.com>
2013-01-11 00:55:31 -0600

    Big huge update
    TODO: Make things not as verbose and more modular

--- config.js
+++ config.js
@@ -1,19 +1,67 @@
 module.exports = {
-    'servers': {
-        'web': {
-            url: 'http://sdelements.com/'
+    'iteration': {
+        'version': '1.0',
+        'description': 'Workin\' on stuffs'
+    },
+    'events': {
+        'event-1': {
+            date: 'Jan 22, 2020',
+            name: 'Big Huge Meeting'
         },
+        'event-2': {
+            date: 'April 27, 2025',
+            name: 'Epic Dance Off'
+        },
+        'event-3': {
+            date: 'March 4, 2040',
+            name: 'Alien Invasion'
+        }
+    },
+    'support': {
+        'houssam': {
+            name: 'Houssam Haidar',
+            gravatar: 'bcea6b8c06220e389093f36a8895fab4',
+            tier: 'I'
+        },
+        'ramanan': {
+            name: 'Ramanan Sivaranjan',
+            gravatar: '92cd9a233c83fdd1aed9fe833b11d1ea',
+            tier: 'II'
+        }
+    },
+    'production-servers': {
         'google': {
-            url: 'http://google.com/',
+            name: 'Google',
+            url: 'http://google.com/'
+        },
+        'github': {
+            name: 'Github',
+            url: 'http://github.com/',
+            test: function(res, body) {
+                return body.match(/somethingbogus/i) ? true : false;
+            }
+        },
+        'demiba': {
+            name: 'Demiba',
+            url: 'http://demiba.com/'
+        }
+    },
+    'internal-servers': {
+        'google': {
+            name: 'Google Canada',
+            url: 'http://google.ca/',
             test: function(res, body) {
                 return body.match(/Feeling Lucky/i) ? true : false;
             }
         },
-        'demiba': {
-            url: 'http://demiba.com/',
-            test: function(res, body) {
-                return body.match(/1 Billion/i) ? true : false;
-            }
+        'example': {
+            url: 'http://example.com/'
         }
+    },
+    'web-servers': {
+        'local': {
+            name: 'Local Web Server',
+            url: 'http://127.0.0.1'
+        },
     }
 }
--- widgets.js
+++ widgets.js
@@ -4,39 +4,57 @@ var request = require('request');
 
 var config = require('./config.js');
 
-module.exports = {
-    'servers': {
-        title: 'Server Status',
-        interval: 30 * 1000,
-        fetch: function(emitter) {
-            var requests = [];
-            var servers = config.servers;
-            _.each(servers, function(server, id) {
-                requests.push(function(callback) {
-                    var current = { 
-                        id: id,
-                        name: server.name,
-                        status: 'up'
-                    }
-                    request({
-                        uri: server.url,
-                        method: server.method || 'GET'
-                    }, function (err, res, body) {
-                        if (err || server.test && !server.test(res, body)) {
-                            // Well shit
-                            current.status = 'down'
-                            callback(null, current);
-                            return;
-                        }
-                        callback(null, current)
-                    });
-                });
+
+var checkServers = function(servers, emitter) {
+    var requests = [];
+    var servers = servers;
+    _.each(servers || false, function(server, id) {
+        requests.push(function(callback) {
+            var current = { 
+                id: id,
+                name: server.name,
+                status: 'up'
+            }
+            request({
+                uri: server.url,
+                method: server.method || 'GET'
+            }, function (err, res, body) {
+                if (err || server.test && !server.test(res, body)) {
+                    // Well shit
+                    current.status = 'down'
+                    callback(null, current);
+                    return;
+                }
+                callback(null, current)
             });
-            async.series(requests, function(err, res) {
-                var data = {};
-                data.servers = res;
-                emitter(data);
-            });
-        }
-    }
+        });
+    });
+    async.series(requests, function(err, res) {
+        var data = {};
+        data.servers = res;
+        emitter(data);
+    });
 }
+
+module.exports = {
+    'production-servers': {
+        interval: 120 * 1000,
+        fetch: function(emitter) {
+            checkServers(config['production-servers'], emitter)
+        }
+    },
+    'internal-servers': {
+        interval: 60 * 1000,
+        fetch: checkServers,
+        fetch: function(emitter) {
+            checkServers(config['internal-servers'], emitter)
+        }
+    },
+    'web-servers': {
+        interval: 60 * 1000,
+        fetch: checkServers,
+        fetch: function(emitter) {
+            checkServers(config['web-servers'], emitter)
+        }
+    },
+} 
